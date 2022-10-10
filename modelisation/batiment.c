b96c285262e09212aade89efb83b3f0ea991055e
raph0603 <raphael.laborie@gmail.com>
2022-10-10 14:07:32 +0200

    Update

---
+++ modelisation/batiment.c
@@ -0,0 +1,51 @@
+// Langage : C
+// Path: batiment.c
+
+#include <stdbool.h> 
+
+struct batiment
+{
+	int id;
+	double consommation;
+	bool etat;
+};
+typedef struct batiment batiment;
+
+batiment *batiment_new(int id, double consommation)
+{
+	batiment *b = malloc(sizeof(batiment));
+	b->id = id;
+	b->consommation = consommation;
+	b->etat = true;
+	return b;
+}
+
+void batiment_free(batiment *b)
+{
+	free(b);
+}
+
+void batiment_set_etat(batiment *b, bool etat)
+{
+	b->etat = etat;
+}
+
+bool batiment_get_etat(batiment *b)
+{
+	return b->etat;
+}
+
+double batiment_get_consommation(batiment *b)
+{
+	return b->consommation;
+}
+
+int batiment_get_id(batiment *b)
+{
+	return b->id;
+}
+
+void batiment_print(batiment *b)
+{
+	printf("Batiment %d : %f %s \r ", b->id, b->consommation, b->etat ? "ON" : "OFF");
+}
---
+++ modelisation/ville.c
@@ -0,0 +1,145 @@
+// Langage : C
+// Path: ville.c
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include "section.h"
+
+struct ville
+{
+	int id;
+	double consommation;
+	bool etat;
+	section **sections;
+};
+
+typedef struct ville ville;
+
+ville *ville_new(int id)
+{
+	ville *v = malloc(sizeof(ville));
+	v->id = id;
+	v->consommation = 0;
+	v->etat = true;
+	v->sections = NULL;
+	return v;
+}
+
+void ville_free(ville *v)
+{
+	free(v);
+}
+
+void ville_set_etat(ville *v, bool etat)
+{
+	v->etat = etat;
+}
+
+bool ville_get_etat(ville *v)
+{
+	return v->etat;
+}
+
+double ville_get_consommation(ville *v)
+{
+	return v->consommation;
+}
+
+int ville_get_id(ville *v)
+{
+	return v->id;
+}
+
+void ville_print(ville *v)
+{
+	printf("Ville %d : %f %s \r ", v->id, v->consommation, v->etat ? "ON" : "OFF");
+}
+
+void ville_add_section(ville *v, section *s)
+{
+	v->sections = realloc(v->sections, sizeof(section *) * (v->id + 1));
+	v->sections[v->id] = s;
+	v->id++;
+}
+
+void ville_print_sections(ville *v)
+{
+	for (int i = 0; i < v->id; i++)
+	{
+		section_print(v->sections[i]);
+	}
+}
+
+void ville_print_batiments(ville *v)
+{
+	for (int i = 0; i < v->id; i++)
+	{
+		section_print_batiments(v->sections[i]);
+	}
+}
+
+void ville_set_etat_sections(ville *v, bool etat)
+{
+	for (int i = 0; i < v->id; i++)
+	{
+		section_set_etat(v->sections[i], etat);
+	}
+}
+
+void ville_set_etat_batiments(ville *v, bool etat)
+{
+	for (int i = 0; i < v->id; i++)
+	{
+		section_set_etat_batiments(v->sections[i], etat);
+	}
+}
+
+void ville_set_consommation(ville *v)
+{
+	double consommation = 0;
+	for (int i = 0; i < v->id; i++)
+	{
+		consommation += section_get_consommation(v->sections[i]);
+	}
+	v->consommation = consommation;
+}
+
+void ville_set_consommation_sections(ville *v)
+{
+	for (int i = 0; i < v->id; i++)
+	{
+		section_set_consommation(v->sections[i]);
+	}
+}
+
+void ville_set_consommation_batiments(ville *v)
+{
+	for (int i = 0; i < v->id; i++)
+	{
+		section_set_consommation_batiments(v->sections[i]);
+	}
+}
+
+void ville_add_batiment(ville *v, batiment *b)
+{
+	v->sections = realloc(v->sections, sizeof(batiment *) * (v->id + 1));
+	v->sections[v->id] = b;
+	v->id++;
+}
+
+void ville_add_batiment_section(ville *v, batiment *b, int id_section)
+{
+	section_add_batiment(v->sections[id_section], b);
+}
+
+void ville_add_section_batiment(ville *v, section *s, int id_batiment)
+{
+	batiment_add_section(v->sections[id_batiment], s);
+}
+
+void ville_add_batiment_section_batiment(ville *v, batiment *b, int id_section, int id_batiment)
+{
+	batiment_add_section(v->sections[id_batiment], s);
+	section_add_batiment(v->sections[id_section], b);
+}
+
